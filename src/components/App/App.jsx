28de0c32c829d380d557428094ef7fd8b1a0a859
Mikhail Mamayeu <mamayeu.mikhail@gmail.com>
2022-07-09 15:59:22 +0200

    chore(setup): :wrench: Set up React application

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Performance Tips</h1>
+      </header>
+      <main>
+        <h2>Parcel is your new best friend</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+          ducimus laborum possimus, quis sed inventore voluptate quod blanditiis
+          illo expedita adipisci itaque, dolorum molestiae asperiores. Nesciunt
+          aspernatur possimus eos debitis!
+        </p>
+      </main>
+      <footer>
+        <small>All rights reserved</small>
+      </footer>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-document.getElementById('app').innerText = 'Performance Tips';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
