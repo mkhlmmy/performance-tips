29df278d40a36600329fa6ffbf7d7e6f3706a91c
Mikhail Mamayeu <mamayeu.mikhail@gmail.com>
2022-07-10 17:07:09 +0200

    feat: :sparkles: Add different ways to load an image to reflect LCP changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,78 @@
+import * as styles from './App.module.css';
+
+const heroImage = new URL(
+  '../../images/hero-image.jpg?width=375&height=250',
+  import.meta.url
+);
+
 function App() {
   return (
-    <>
-      <header>
-        <h1>Performance Tips</h1>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <nav>
+          <ul className={styles['nav-list']}>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li>Dolor</li>
+            <li>Sit</li>
+            <li>Amet</li>
+          </ul>
+        </nav>
       </header>
-      <main>
-        <h2>Parcel is your new best friend</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          ducimus laborum possimus, quis sed inventore voluptate quod blanditiis
-          illo expedita adipisci itaque, dolorum molestiae asperiores. Nesciunt
-          aspernatur possimus eos debitis!
-        </p>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          <h1 className={styles['primary-heading']}>
+            Lorem ipsum dolor sit amet
+          </h1>
+          <section>
+            <h2 className={styles['secondary-heading']}>
+              Lorem ipsum dolor sit amet
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Laudantium ducimus laborum possimus, quis sed inventore voluptate
+              quod blanditiis illo expedita adipisci itaque, dolorum molestiae
+              asperiores. Nesciunt aspernatur possimus eos debitis!
+            </p>
+          </section>
+        </article>
+        {process.env.BUILD_AS === 'img' && (
+          <img
+            src={heroImage}
+            alt="Lorem ipsum dolar sit"
+            width="375"
+            height="250"
+            className={styles['hero-image']}
+          />
+        )}
+        {process.env.BUILD_AS === 'svg-image' && (
+          <svg
+            width="375"
+            height="250"
+            xmlns="http://www.w3.org/1000/svg"
+            viewBox="0 0 375 250"
+            className={styles['hero-image']}
+          >
+            <image href={heroImage} width="375" height="250" />
+          </svg>
+        )}
+        {process.env.BUILD_AS === 'video-poster' && (
+          // eslint-disable-next-line jsx-a11y/media-has-caption
+          <video
+            poster={heroImage}
+            width="375"
+            height="250"
+            className={styles['hero-image']}
+          />
+        )}
+        {process.env.BUILD_AS === 'background-image' && (
+          <div className={` ${styles['hero-image-background']}`} />
+        )}
       </main>
-      <footer>
-        <small>All rights reserved</small>
+      <footer className={styles.footer}>
+        <small>Lorem ipsum, dolor sit amet consectetur adipisicing elit</small>
       </footer>
-    </>
+    </div>
   );
 }
 
